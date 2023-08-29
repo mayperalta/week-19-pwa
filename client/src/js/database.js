1d63d48c806f5ad295dc45e19def453e6dc88092
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-08-29 15:48:05 -0400

    Edit JS - fix dependencies

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,19 +17,19 @@ const initdb = async () =>
 export const putDb = async (content)  => {
   console.log('PUT to the database');
 
-  // Create a connection to the database database and version we want to use.
+  // Create a connection to DB
   const contactDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  // Create a new transaction, specify DB, and data privileges.
+  const newTrasaction = contactDb.transaction('jate', 'readwrite');
 
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  // Open object store.
+  const store = newTrasaction.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
   const request = store.put({ id: 1, value: content });
 
-  // Get confirmation of the request.
+  // Get confirmation of request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -40,18 +40,11 @@ export const putDb = async (content)  => {
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
+  const newTrasaction = contactDb.transaction('jate', 'readonly');
+  const store = newTrasaction.objectStore('jate');
+  // Use the .getAll() method to get all data in the DB
   const request = store.getAll();
-
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
